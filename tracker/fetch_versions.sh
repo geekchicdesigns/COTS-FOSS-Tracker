1c55d47595988baf0d3ee6560535278598332329
Kdaake <kdaake@yahoo.com>
2026-01-03 16:24:44 -0700

    wiring ArgoCD

--- tracker/fetch_versions.sh
+++ tracker/fetch_versions.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
-
 set -e
 
-argocd app list -o json > tracker/output/argocd_apps.json
+# 1. What Argo CD knows about apps (deployed intent + health)
+argocd app list -o json > output/argocd_apps.json
 
+# 2. What upstream versions exist (informational)
 helm repo update
-helm search repo argo-cd -o json > tracker/output/helm_versions.json
+helm search repo argo-cd -o json > output/helm_versions.json
+
+# 3. What THIS PROGRAM declares as approved (authoritative)
+yq -o=json ../argocd/values/*.yaml > output/desired_versions.json
 
